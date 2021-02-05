858ca06cc1b8b5b0afaac3e07bcf1ff6ebff554e
MeoBeoI <quy.tran2001100@vnuk.edu.vn>
2021-02-05 16:50:50 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-await-in-loop */
 /* eslint-disable max-len */
 const axios = require('axios');
 
@@ -14,16 +15,42 @@ async function getTotalScore(team, year) {
     headers: { },
   };
 
-  const responseHome = await axios(configHome);
-  const responseAway = await axios(configAway);
+  const responseHomeInit = await axios(configHome);
+  const responseAwayInit = await axios(configAway);
 
-  const totalGoalsHome = responseHome.data.data.reduce((total, cur) => Number(total) + Number(cur.team1goals), 0);
-  const totalGoalsAway = responseAway.data.data.reduce((total, cur) => Number(total) + Number(cur.team2goals), 0);
+  let arrayHomeMatchs = responseHomeInit.data.data;
+  let arrayAwayMatchs = responseAwayInit.data.data;
+  // If more than 1 page, append data array
+  if (responseHomeInit.data.total_pages !== 1) {
+    for (let i = 2; i <= responseHomeInit.data.total_pages; i++) {
+      const returnData = await axios({
+        method: 'get',
+        url: `http://jsonmock.hackerrank.com/api/football_matches?year=${year}&team1=${team}&page=${i}`,
+        headers: { },
+      });
+      arrayHomeMatchs = arrayHomeMatchs.concat(returnData.data.data);
+    }
+  }
+
+  // If more than 1 page, append data array
+  if (responseAwayInit.data.total_pages !== 1) {
+    for (let i = 2; i <= responseHomeInit.data.total_pages; i++) {
+      const returnData = await axios({
+        method: 'get',
+        url: `http://jsonmock.hackerrank.com/api/football_matches?year=${year}&team2=${team}&page=${i}`,
+        headers: { },
+      });
+      arrayAwayMatchs = arrayAwayMatchs.concat(returnData.data.data);
+    }
+  }
+
+  const totalGoalsHome = arrayHomeMatchs.reduce((total, cur) => Number(total) + Number(cur.team1goals), 0);
+  const totalGoalsAway = arrayAwayMatchs.reduce((total, cur) => Number(total) + Number(cur.team2goals), 0);
 
   return totalGoalsHome + totalGoalsAway;
 }
 
 (async () => {
-  const total = await getTotalScore('Barcelona', 2011);
+  const total = await getTotalScore('Chelsea', 2014);
   console.log(total);
 })();
